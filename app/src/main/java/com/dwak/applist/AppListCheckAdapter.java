580c7d6b98d2edc1d1e0857ecd836d0ec002ecdf
Vishnu Rajeevan <vishnurajeevan@gmail.com>
2014-08-07 23:03:18 -0500

    adding export to file

---
+++ app/src/main/java/com/dwak/applist/AppListCheckAdapter.java
@@ -0,0 +1,33 @@
+package com.dwak.applist;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class AppListCheckAdapter extends ArrayAdapter{
+    private final List<AppListItem> mItems;
+    private final Context mContext;
+
+    public AppListCheckAdapter(Context context, int resource, List<AppListItem> items) {
+        super(context, resource, items);
+        mItems = items;
+        mContext = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.list_item_checkbox, parent, false);
+        TextView textView = (TextView) rowView.findViewById(R.id.item_name);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.item_icon);
+        textView.setText(mItems.get(position).getApplicationName());
+        imageView.setBackground(mItems.get(position).getApplicationIconDrawable());
+        return rowView;
+    }
+}
